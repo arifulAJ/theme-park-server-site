77116d8b46af580285ad81f92cfb648fe6c264ee
arifulislam10111999 <arifulislam10111999@gmail.com>
2021-10-30 22:41:46 +0600

    port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const cors = require('cors')
 const app=express();
 app.use(cors())
-const port=5000;
+const port=process.env.PORT|| 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lreh2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
